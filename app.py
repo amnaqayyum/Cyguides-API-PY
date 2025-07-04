51e8867944dd51f285463cd4c1a60d0e300800f1
Amna <amna_chohan@hotmail.com>
2025-07-04 21:46:31 +0500

    port

--- app.py
+++ app.py
@@ -27,6 +27,5 @@ def ask():
             "error": str(e)
         }), 500
 
-# For Railway
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
